c1aff8941b9d4e0c6aecda1e58a71e10b71221c5
maryam alarcon <23221474+malarcon1@users.noreply.github.com>
2018-04-16 19:52:52 -0400

    removed a comment

--- server/main.js
+++ server/main.js
@@ -5,7 +5,7 @@ import { Promise } from 'meteor/promise';
 //Resolutions = new Mongo.Collection('resolutions');
 
 Meteor.startup(() => {
-  // code to run on server at startup
+
 
 //      process.env.MAIL_URL = "smtp://postmaster@sandboxa49634e118e44b50bccf99a45000d56b.mailgun.org:725f48281cab4e9ec7130b8913aa0822-21e977f8-cbb3af54@smtp.mailgun.org:587";
 
