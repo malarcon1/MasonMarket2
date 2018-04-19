4f87ffc85dd76ad6755f34e98f7004032cd4d97b
ksyedabu1 <36109203+ksyedabu1@users.noreply.github.com>
2018-04-19 17:11:33 -0400

    Search by title only
    Note: 
    -search.html AND search.js (client) goes in client folder
    -search.js (server) ==> server folder
    (I specified which one is client and server in file at the top)

--- client/search.js
+++ client/search.js
@@ -1,57 +1,28 @@
+//This file goes into your SERVER folder
+
+import { Meteor } from 'meteor/meteor';
 import {Template} from 'meteor/templating';
-import './login.html';
 import { Books } from '../lib/collections.js';
-import {BookContract} from "../contracts/book_contract.js";
 
-Template.search.events({
-    'click .logout': function (event) {
-        event.preventDefault();
-        console.log("Test");
-        Meteor.logout();
-    },
-    'keyup [name="search"]' ( event, template ) {
-        let value = event.target.value.trim();
 
-        if ( value !== '' && event.keyCode === 13 ) {
-            template.searchQuery.set( value );
-            template.searching.set( true );
-        }
+if (Meteor.isServer){
+	Meteor.startup(function (){
+		Books._ensureIndex({
+			"title": "text",
+		});
+	//	seed();	
+	});
+	
+	
+	Meteor.publish("search", function(searchVal){
+	if (!searchVal){
+		return Books.find({});
+	}
+	
+	console.log("Search item", searchVal);
+	
+	return Books.find(
+		{ $text: {$search: searchVal} } );
+	});
+}
 
-        if ( value === '' ) {
-            template.searchQuery.set( value );
-        }
-    }
-});
-
-Template.search.onCreated( () => {
-    let template = Template.instance();
-
-    template.searchQuery = new ReactiveVar();
-    template.searching   = new ReactiveVar( false );
-
-    template.autorun( () => {
-        template.subscribe( 'books', template.searchQuery.get(), () => {
-            setTimeout( () => {
-                template.searching.set( false );
-            }, 300 );
-        });
-    });
-});
-
-Template.search.helpers({
-    firstName: function () {
-        return Meteor.user().emails[0].address;
-    },
-    searching() {
-        return Template.instance().searching.get();
-    },
-    query() {
-        return Template.instance().searchQuery.get();
-    },
-    books() {
-        let books = Books.find();
-        if ( books ) {
-            return books;
-        }
-    }
-});
