28985ddda22b25ad7025208df84fa5ad71fb1547
Hamza-Mughal <hmughal2@masonlive.gmu.edu>
2018-04-18 20:41:16 -0400

    Displays info from DB on listing.

--- client/listing.js
+++ client/listing.js
@@ -6,6 +6,10 @@ import './createlisting.html';
 import {BookContract} from "../contracts/book_contract.js";
 import { Books } from '../lib/collections.js';
 
+Template.listing.onCreated(function() {
+
+    this.subscribe('books');
+});
 
 
 
@@ -16,6 +20,27 @@ Template.listing.events({
         var id = Router.current().params.id; // get ID
         var collection = Books.findOne({_id: id}); // query with ID
         console.log(collection);
+
     }
 });
 
+
+Template.listing.helpers({
+
+    books: function(){
+
+        // this is how to create and insert a new book
+        // let myBook = new BookContract(
+        //     title="Book from server code",
+        //     price=19.99,
+        //     description="This is a random book.",
+        //     imagesrc="book.jpg",
+        //     owner="user@masonlive.gmu.edu",
+        //     condition="new");
+        // Books.insert(myBook);
+        var id = Router.current().params.id; // get ID
+        var collection = Books.find({_id: id}); // query with ID
+        return collection;
+    }
+
+});
