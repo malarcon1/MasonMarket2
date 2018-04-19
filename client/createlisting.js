0d9906a1a5774e4d6fe526ca2923001e57a49e82
ksyedabu1 <36109203+ksyedabu1@users.noreply.github.com>
2018-04-18 21:07:40 -0400

    Updated version

--- client/createlisting.js
+++ client/createlisting.js
@@ -9,7 +9,7 @@ import { Books } from '../lib/collections.js';
 
 
 Template.createlisting.onCreated(function onCreated(){
-	this.subscribe('Books');
+	this.subscribe('books');
 
 });
 
@@ -17,7 +17,8 @@ Template.createlisting.onCreated(function onCreated(){
 Template.createlisting.helpers({
   firstName: function() {
     return Meteor.user().emails[0].address;
-  }
+  }			
+ 
 });
 
 		
@@ -35,18 +36,57 @@ Template.createlisting.events({
 		event.preventDefault();
 		var title = $('[name=listname]').val();
 		var price = $('[name=price]').val();
-		var condition = $('[name=condition]').val();
+		var condition = $('.form-check-input:checked').val();
 		var course = $('[name=course]').val();
 		var imagesrc = $('[name=pic]').val();
 		var description = $('[name=description]').val();
-		var owner = Meteor.user().emails[0].address;
+		var owner = Meteor.user();
 		var createdAt = new Date();
 		
+		console.log(imagesrc);
+		
+		/*uploading images via cloudinary
+		var files = [];
+		var imagesrc = $('[name=pic]').val();
+		files.push(imagesrc);
+		console.log("Image is" + files[0]);
+		
+		Cloudinary.upload(files[0], function(err,res){
+			console.log("Upload Error:" + err);
+			console.log("Result:" + res);
+		});
+		
+		console.log("finished" + files[0]);*/
+			
+		
+		
+		if (price < 0 || price > 5000)
+		{
+				alert("Price out of range! Please give valid price from 0 to 5000");
+				return;
+		}
+	
+		if (condition == undefined)
+		{
+				alert("Please choose a condition for the book!");
+				return;
+		}
+		
+		else
+		{
+		
+			console.log("User created listing");
 				
-		let listing = new BookContract(title, price, description, imagesrc, owner, condition, course);
+			let listing = new BookContract(title, price, description, imagesrc, owner, condition, course);
 		
 		//insert into book collection
-		Books.insert(listing);
+			Books.insert(listing);
+		
+			console.log(listing);
+		
+			Router.go('home');
+			
+		}
 	}
 	
 });
