a9d68c70e37a951914abdfb9599f0017767c26d4
ksyedabu1 <36109203+ksyedabu1@users.noreply.github.com>
2018-04-19 21:32:37 -0400

    Updated Search (most recent, better one)
    (I just placed the search.js in this "server" folder to separate it from client search.js, though this server folder is not the one part of our app/website)

---
+++ server/search.js
@@ -0,0 +1,28 @@
+//This file goes into your SERVER folder
+
+import { Meteor } from 'meteor/meteor';
+import {Template} from 'meteor/templating';
+import { Books } from '../lib/collections.js';
+
+
+if (Meteor.isServer){
+	Meteor.startup(function (){
+		Books._ensureIndex({
+			"title": "text"
+		});
+	//	seed();	
+	});
+	
+	
+	Meteor.publish("search", function(searchVal){
+	if (!searchVal){
+		return Books.find({});
+	}
+	
+	console.log("Search item", searchVal);
+	
+	return Books.find(
+		{ $text: {$search: searchVal} } );
+	});
+}
+
